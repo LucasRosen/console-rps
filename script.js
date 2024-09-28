dee620afaf4830eb88f8db650eb4a304c73fbd80
Lucas Rosén <49569378+LucasRosen@users.noreply.github.com>
2024-09-28 21:57:02 +0200

    Code the UI as well as very small adjusments in style.css

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const choices = ["rock", "paper", "scissors"];
 
+function capitalizeFirst(text) {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function getNewStatus(humanScore, computerScore) {
-  if (humanScore === computerScore) {
-    return `You tied! ${humanScore} - ${computerScore}`;
-  } else if (humanScore > computerScore) {
-    return `You won! ${humanScore} - ${computerScore}`;
+function updateStatus(winner, humanChoice, computerChoice) {
+  const capsHumanChoice = capitalizeFirst(humanChoice);
+  const capsComputerChoice = capitalizeFirst(computerChoice);
+
+  if (winner === "human") {
+    statusText.textContent = `${capsHumanChoice} beats ${computerChoice}! \nYou win!`;
+  } else if (winner === "computer") {
+    statusText.textContent = `${capsComputerChoice} beats ${humanChoice}! \nYou lose!`;
   } else {
-    return `You lost! ${humanScore} - ${computerScore}`;
+    statusText.textContent = "Tie!";
   }
+}
+
+function updateScoreboard() {
+  scoreboard.textContent = `${humanScore} - ${computerScore}`
 }
 
 function getWinner(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
-    console.log("Tie");
+    return "tie";
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "paper" && computerChoice === "rock") ||
@@ -27,3 +38,33 @@ function getWinner(humanChoice, computerChoice) {
     return "computer";
   }
 }
+
+function playRound(humanChoice, computerChoice) {
+  const winner = getWinner(humanChoice, computerChoice);
+  if (winner === "human") {
+    humanScore++;
+    updateStatus(winner, humanChoice, computerChoice);
+  } else if (winner === "computer") {
+    computerScore++;
+    updateStatus(winner, humanChoice, computerChoice);
+  } else {
+    updateStatus(winner, humanChoice, computerChoice);
+  }
+
+  updateScoreboard();
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const statusText = document.querySelector("#status");
+const scoreboard = document.querySelector("#scoreboard");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    let humanChoice = event.target.id;
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  });
+});
